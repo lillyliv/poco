02b44f7c0752afb2ede5ad2ac53a0f41afd0c11a
lillyliv <lillyliv1245@gmail.com>
2021-09-11 11:32:51 -0400

    Add files via upload

---
+++ HandleInput.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poco
+{
+    class HandleInput
+    {
+        public static void main()
+        {
+            if ((Program.cki.Modifiers & ConsoleModifiers.Alt) != 0)
+            {
+                Program.alt = true;
+            }
+            else
+            {
+                Program.alt = false;
+            }
+            if ((Program.cki.Modifiers & ConsoleModifiers.Shift) != 0)
+            {
+                Program.shift = true;
+            }
+            else
+            {
+                Program.shift = false;
+            }
+            if ((Program.cki.Modifiers & ConsoleModifiers.Control) != 0)
+            {
+                Program.control = true;
+            }
+            else
+            {
+                Program.control = false;
+            }
+            if (Program.keyInfo.Key == ConsoleKey.Backspace && Program.cursorx != 0)
+            {
+                consoleStuff.writeAt(" ", Program.cursorx - 1, Program.cursory);
+                Program.cursorx -= 1;
+                Console.SetCursorPosition(Program.cursorx, Program.cursory);
+            }
+            else if (Program.keyInfo.Key == ConsoleKey.Backspace && Program.cursorx == 0 && Program.cursory != 0)
+            {
+                consoleStuff.writeAt(" ", Program.cursorx, Program.cursory + 1);
+                Program.cursory -= 1;
+
+            }
+            else if (Program.keyInfo.Key == ConsoleKey.Enter)
+            {
+                consoleStuff.writeAt("\n", Program.cursorx, Program.cursory);
+                Program.cursorx = 0;
+                Program.cursory += 1;
+            }
+            else if (Program.keyInfo.Key == ConsoleKey.LeftArrow)
+            {
+                if (Program.cursorx == 0) { }
+                else
+                {
+                    Console.SetCursorPosition(Program.cursorx - 1, Program.cursory);
+                    Program.cursorx--;
+                }
+
+            }
+            else if (Program.keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                if (Program.cursorx == Program.height - 1) { }
+                else
+                {
+                    Console.SetCursorPosition(Program.cursorx, Program.cursory + 1);
+                    Program.cursory++;
+                }
+
+            }
+            else if (Program.keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                if (Program.cursory == 0) { }
+                else
+                {
+                    Console.SetCursorPosition(Program.cursorx, Program.cursory - 1);
+                    Program.cursory--;
+                }
+
+            }
+            else if (Program.keyInfo.Key == ConsoleKey.RightArrow)
+            {
+                if (Program.cursorx == Program.width - 1) { }
+                else
+                {
+                    Console.SetCursorPosition(Program.cursorx + 1, Program.cursory);
+                    Program.cursorx++;
+                }
+
+            }
+            else
+            {
+                consoleStuff.writeAt(Program.key.ToString(), Program.cursorx, Program.cursory);
+                Program.cursorx++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace poco
+{
+    class consoleStuff
+    {
+
+        public static int origRow = 0;
+        public static int origCol = 0;
+
+        public static void writeAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class Program
+    {
+        public static bool alt = false;
+        public static bool shift = false;
+        public static bool control = false;
+        /*
+        mode: 0 = type
+        mode: 1 = normal
+        */
+        public static byte mode = 1;
+        public static bool toQuit = false;
+
+        public static ushort width;
+        public static ushort height;
+        public static ushort cursorx = 0;
+        public static ushort cursory = 0;
+
+        public static char key;
+        public static ConsoleKeyInfo cki = new ConsoleKeyInfo();
+        public static ConsoleKeyInfo keyInfo;
+
+        static void Main(string[] args)
+        {
+            //consoleStuff.writeAt("hello world", 0, 0);
+            do
+            {
+                while (Console.KeyAvailable == false)
+                    Thread.Sleep(10); // Loop until input is entered.
+                cki = Console.ReadKey(true);
+
+                width = (ushort)Console.WindowWidth;
+                height = (ushort)Console.WindowHeight;
+                keyInfo = cki;
+                key = keyInfo.KeyChar;
+
+
+                HandleInput.main();
+
+            } while (toQuit == false);
+        }
+    }
+}
